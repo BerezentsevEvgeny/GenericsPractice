00c71df2be1d8c667fe06709c0b7bf800b06723e
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-22 21:02:01 +0300

    Initial Commit

---
+++ GenericsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericsPractice
+//
+//  Created by Евгений Березенцев on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
